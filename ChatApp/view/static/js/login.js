b68da63e3abb210f4184141992318af733e2114e
oyama <kamome2655@gmail.com>
2023-08-13 07:08:39 +0900

    login.htmlのテンプレ化とjs修正

--- ChatApp/view/static/js/login.js
+++ ChatApp/view/static/js/login.js
@@ -1,11 +1,17 @@
-$(function() {
-    $('.signup-show').click(function() {
-      $('#signup-modal').fadeIn();
+// ページが読み込まれたときの処理
+document.addEventListener('DOMContentLoaded', function() {
+    const signupShowButton = document.querySelector('.signup-show');
+    const signupModal = document.getElementById('signup-modal');
+    const closeModalButton = document.getElementById('close-modal');
+
+    // モーダル表示ボタンがクリックされたときの処理
+    signupShowButton.addEventListener('click', function() {
+        signupModal.style.display = 'block';
     });
-    $('#close-modal').click(function() {
-      $('#signup-modal').fadeOut();
+
+    // モーダル閉じるボタンがクリックされたときの処理
+    closeModalButton.addEventListener('click', function() {
+        signupModal.style.display = 'none';
     });
-  
-  
-  });
+});
 
