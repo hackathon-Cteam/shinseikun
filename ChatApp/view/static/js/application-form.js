af5994a5b76fa354ec16bf4be6b9d64d4a2737b4
dbvt19 <93956271+dbvt19@users.noreply.github.com>
2023-08-13 19:56:14 +0900

    jsのかわりに一旦htmlで入力必須判定機能を実装

--- ChatApp/view/static/js/application-form.js
+++ ChatApp/view/static/js/application-form.js
@@ -1,14 +1,36 @@
-function check(){
-  if (
-      application_form.purpose.value == "" ||
-      application_form.name.value == "" ||
-      application_form.email.value == "" ||
-      application_form.phone.value == ""
-  ){
-      //上記条件に一致する場合
-      alert("空欄のある項目があります");    //エラーメッセージを出力
-      return false;    //送信ボタン本来の動作をキャンセルします
-  }else{
-      return true;    //送信ボタン本来の動作を実行します
-  }
+// /**
+//  * 入力エラーチェック
+//  */
+// function checkInput() {
+//   if (
+//       application_form.purpose.value == "" ||
+//       application_form.name.value == "" ||
+//       application_form.email.value == "" ||
+//       application_form.phone.value == ""
+//   ){
+//       //上記条件に一致する場合
+//       alert("空欄もしくは未選択の項目があります");    //エラーメッセージを出力
+//       return false;    //送信ボタン本来の動作をキャンセルします
+//   } else {
+//       return true;    //送信ボタン本来の動作を実行します
+//   }
+// }
+
+/**
+ * 施設名の絞り込み検索
+ */
+
+function sortFacility() {
+    const sortKeyword = document.getElementById("sortKeyword").value.toUpperCase();    //検索フォームに入力されたキーワードを取得（大文字に揃える）
+    const facilityList = document.getElementsById("facilityList");    //選択肢（選択ボタン＋施設名）を取得
+    const facilityName = document.getElementsById("facilityName");    //施設名を取得
+
+      for (i = 0; i < facilityList.length; i++) {    //選択肢の長さ分繰り返し
+        value = facilityName[i].textContent;    //施設名の値を変数に代入
+        if (value.toUpperCase().indexOf(sortKeyword) > -1) {    //施設名の値とキーワードが不一致でなければ（施設名は大文字に揃える）
+          facilityList[i].style.display = "";    //選択肢を表示
+        } else {
+          facilityList[i].style.display = "none";   //その他の場合は選択肢を非表示
+        }
+      }
 }
