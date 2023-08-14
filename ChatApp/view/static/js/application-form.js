669f260efb2e772639ebba6a62e2a6f89df25429
dbvt19 <93956271+dbvt19@users.noreply.github.com>
2023-08-14 20:02:28 +0900

    loadイベント,addEventListenerの適用とフォーム送信時の処理(たたき台)作成

--- ChatApp/view/static/js/application-form.js
+++ ChatApp/view/static/js/application-form.js
@@ -16,21 +16,47 @@
 //   }
 // }
 
+
+
 /**
- * 施設名の絞り込み検索
+ * ページ読み込み時の処理
  */
 
-function sortFacility() {
-    const sortKeywordValue = document.getElementById("sortKeyword").value.toUpperCase();    //検索フォームに入力されたキーワードをの値を取得（大文字に揃える）
-    const facilityList = document.getElementsByClassName("facilityList");    //選択肢（選択ボタン＋施設名）を取得
-    const facilityName = document.getElementsByClassName("facilityName");    //施設名を取得
+//検索ボタンとフォーム要素を取得
+window.addEventListener('load', function() {
+  const sortButton = document.getElementById("sortButton");
+  const form = document.querySelector("#form");
 
-      for (i = 0; i < facilityList.length; i++) {    //選択肢の個数分繰り返し
-        facilityNameValue = facilityName[i].textContent;    //施設名の値を変数に代入
-        if (facilityNameValue.toUpperCase().indexOf(sortKeywordValue) > -1) {    //施設名の値とキーワードの値が不一致でなければ（施設名の値は大文字に揃える）
-          facilityList[i].style.display = "";    //選択肢を表示
-        } else {
-          facilityList[i].style.display = "none";   //その他の場合は選択肢を非表示
-        }
+    //検索ボタンクリック時の処理を定義
+    sortButton.addEventListener("click", function() {
+      sortFacility();    //施設名の絞り込み検索を実行
+      console.log("検索の実行");    //挙動確認用
+    })
+
+    //フォーム送信時の処理を定義
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      console.log("モーダルの立ち上げ");    //挙動確認用
+      //※モーダル立ち上げ処理を実装予定
+    })
+})
+
+
+
+/**
+ * 施設名の絞り込み検索機能
+ */
+function sortFacility() {
+  const sortKeywordValue = document.getElementById("sortKeyword").value.toUpperCase();    //検索フォームに入力されたキーワードをの値を取得（大文字に揃える）
+  const facilityList = document.getElementsByClassName("facilityList");    //選択肢（選択ボタン＋施設名）の要素を取得
+  const facilityName = document.getElementsByClassName("facilityName");    //施設名の要素を取得
+
+    for (i = 0; i < facilityList.length; i++) {    //選択肢の要素の個数分繰り返し
+      facilityNameValue = facilityName[i].textContent;    //施設名の要素から値を抽出し変数に代入
+      if (facilityNameValue.toUpperCase().indexOf(sortKeywordValue) > -1) {    //施設名の値とキーワードの値が不一致でなければ（施設名の値は大文字に揃える）
+        facilityList[i].style.display = "";    //選択肢の要素を表示
+      } else {
+        facilityList[i].style.display = "none";   //その他の場合は選択肢の要素を非表示
       }
+    }
 }
