5813f4217fabcc637b52ccfa5d184994b2fdfa1c
dbvt19 <93956271+dbvt19@users.noreply.github.com>
2023-08-16 18:48:38 +0900

    確認モーダルsubmit時の処理を追加

--- ChatApp/view/static/js/application-form.js
+++ ChatApp/view/static/js/application-form.js
@@ -17,19 +17,21 @@
 // }
 
 /**
- * ページ読み込み時の処理
+ * ページ読み込み時に実施する処理
  */
 window.addEventListener('load', () => {
-    //施設名の絞り込み検索機能の登録処理（別途記載）を実行
+    //施設名の絞り込み検索機能の登録（別途記載）を実行
     registerSortFacilityEvent();
-    //確認モーダル立ち上げ機能の登録処理（別途記載）を実行
+    //確認モーダルを立ち上げる機能の登録（別途記載）を実行
     registerCreateConfirmModalEvent();
-    //確認モーダルを閉じる機能の登録処理（別途記載）を実行
+    //確認モーダルを閉じる機能の登録（別途記載）を実行
     registerCloseConfirmModalEvent();
+    //確認モーダルをsubmitした際の処理の登録（別途記載）を実行
+    registerSubmitConfirmModalEvent();
 })
 
 /**
- * 施設名の絞り込み検索機能の登録処理
+ * 施設名の絞り込み検索機能の登録
  */
 function registerSortFacilityEvent() {
   //検索ボタンを取得
@@ -42,33 +44,50 @@ function registerSortFacilityEvent() {
 }
 
 /**
- * 確認モーダル立ち上げ機能の登録処理
+ * 確認モーダルを立ち上げる機能の登録
  */
 function registerCreateConfirmModalEvent() {
-  //フォーム要素を取得
+  //親画面のフォーム要素を取得
   const form = document.querySelector("#form");
   //確認モーダル要素を取得
   const confirmModal = document.getElementById('confirm-modal');
-    //フォーム送信時の処理
+    //親画面のフォームのsubmit時
     form.addEventListener("submit", function(event) {
-      event.preventDefault();    //フォーム送信を一旦キャンセル
+      event.preventDefault();    //親画面のフォームのsubmitを一旦キャンセル
       confirmModal.style.display = 'block';    //確認モーダルの立ち上げ
       console.log("確認モーダルの立ち上げ");    //挙動確認用
     })
 }
 
 /**
- * 確認モーダル立ち上げ機能の登録処理
+ * 確認モーダルを閉じる機能の登録
  */
 function registerCloseConfirmModalEvent() {
   //確認モーダル要素を取得
   const confirmModal = document.getElementById('confirm-modal');
   //確認モーダルを閉じるボタンを取得
   const closeConfirmModalButton = document.getElementById('confirm-modal-close');
-  // ボタンがクリック時の処理
+    // 閉じるボタンクリック時
     closeConfirmModalButton.addEventListener('click', function() {
       confirmModal.style.display = 'none';   //確認モーダルを閉じる
       console.log("確認モーダルを閉じる");    //挙動確認用
+    })
+}
+
+/**
+ * 確認モーダルをsubmitした際の処理の登録
+ */
+function registerSubmitConfirmModalEvent() {
+  //親画面のフォーム要素を取得
+  const form = document.querySelector("#form");
+  //確認モーダルのフォーム要素を取得
+  const confirmModalForm = document.querySelector("#confirm-modal-form");
+    //確認モーダルのsubmit時
+    confirmModalForm.addEventListener("submit", function(event) {
+      event.preventDefault();    //確認モーダルのフォームのsubmitは行わない
+      form.submit()    //停止していた親画面のフォームのsubmitを実施
+      console.log("親画面のフォームを送信しマイページへ遷移");    //挙動確認用
+      //マイページへ遷移させる処理を書く
     })
 }
 
