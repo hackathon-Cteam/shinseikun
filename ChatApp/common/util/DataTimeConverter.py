518ed78b781e204eff47a4923fcff64fd9fd685f
chihiroSkmt <mosamosa_5k3c_chi@icloud.com>
2023-08-20 22:07:17 +0900

    日時データ変換クラスを作成

---
+++ ChatApp/common/util/DataTimeConverter.py
@@ -0,0 +1,21 @@
+import datetime
+
+class DataTimeConverter:
+    """日時データ変換クラス"""
+
+    def convertDatetime(str):
+        """文字列からDatatimeオブジェクトに変換する
+        
+        Args:
+        * str (String): 日時データを表す文字列
+        """
+        return datetime.datetime.strptime(str, '%Y-%m-%d %H:%M:%S')
+    
+    def convertStr(datetime, isEenableSeconds= False):
+        """Datetimeオブジェクトから文字列(yyyy/mm/dd hh:mm)に変換する
+
+        Args:
+        * datetime (datetime): datetimeオブジェクト
+        * isEenableSeconds (boolean):  秒数まで有効にするかどうか。デフォルトは無効
+        """
+        return datetime.strftime('%Y/%m/%d %H:%M:%S') if isEenableSeconds else datetime.strftime('%Y/%m/%d %H:%M')
