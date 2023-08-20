23fbfdeb9dad78c768a8aa82c7e8f716cd102592
chihiroSkmt <mosamosa_5k3c_chi@icloud.com>
2023-08-20 16:25:27 +0900

    “サービスクラスの削除”

--- ChatApp/model/service/ChannelService.py
+++
@@ -1,8 +0,0 @@
-from model.external.DBManager import DBManager
-from common.ErrorMessage import ErrorMessage
-
-class ChannelService :
-    """チャンネルに関する情報を取得するサービスクラス"""
-
-    """DBのチャンネルに関するテーブル名"""
-    CHANNEL_DB = 'channels'
